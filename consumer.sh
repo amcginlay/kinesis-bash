69a6e6d6a8e0c4a901dd88bbc0fed40c42159ecb
Alan McGinlay <amcginlay@gmail.com>
2020-11-28 14:27:30 +0000

    initial

---
+++ consumer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo "missing arg, please pass numerical stream identifier"
+    exit 0
+fi
+
+stream_name=chat_${1}
+
+echo "waiting: ${stream_name}"
+aws kinesis wait stream-exists --stream-name ${stream_name}
+
+echo "consuming: ${stream_name} (ctrl+c to end)"
+iterator=$(aws kinesis get-shard-iterator --stream-name ${stream_name} --shard-id 0 --shard-iterator-type TRIM_HORIZON --query '[ShardIterator]' --output text)
+for (( c=1; ; c++ )); do
+  echo "iteration: ${c}"
+  results=$(aws kinesis get-records --shard-iterator ${iterator})
+  iterator=$(echo ${results} | jq .NextShardIterator --raw-output)  
+  echo ${results} | jq '.Records[].Data' --raw-output | while read data; do
+    echo $(base64 --decode <<< ${data})
+  done
+done
---
+++ producer.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+stream_id=${1:-${RANDOM}}
+outer_range=${2:-10}
+inner_range=${3:-25}
+sleep_duration=${4:-2}
+
+stream_name=chat_${stream_id}
+
+echo "----------------------------------------------------------"
+echo "start './consumer.sh ${stream_id}' in another terminal session"
+echo "----------------------------------------------------------"
+echo
+
+echo "checking for: ${stream_name}"
+if aws kinesis describe-stream --stream-name ${stream_name} > /dev/null 2>&1; then
+  echo "deleting: ${stream_name}"
+  aws kinesis delete-stream --stream-name ${stream_name}
+  aws kinesis wait stream-not-exists --stream-name ${stream_name}
+  echo "deleted: ${stream_name}"
+fi
+echo "creating: ${stream_name}"
+aws kinesis create-stream --stream-name ${stream_name} --shard-count 1
+aws kinesis wait stream-exists --stream-name ${stream_name}
+echo "created: ${stream_name}"
+
+echo "putting records in: ${stream_name}"
+for outer in $(seq ${outer_range}); do
+  records=""
+  for inner in $(seq ${inner_range}); do
+    record="Data=$(base64 <<< ${outer}.${inner}),PartitionKey=${RANDOM}"
+    records="${records} ${record}"
+  done
+  echo "producing batch: ${outer}"
+  aws kinesis put-records --stream-name ${stream_name} --records ${records} > /dev/null 2>&1
+  echo "sleeping for ${sleep_duration} seconds"
+  sleep ${sleep_duration}
+done
