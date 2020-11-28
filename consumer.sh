38d5d53e54edb6eab8e9215cde896d598486ce6f
Alan McGinlay <amcginlay@users.noreply.github.com>
2020-11-28 15:07:04 +0000

    Update consumer.sh

--- consumer.sh
+++ consumer.sh
@@ -7,16 +7,16 @@ fi
 
 stream_name=data_${1}
 
-echo "waiting for ${stream_name}"
+echo "waiting for stream ${stream_name}"
 aws kinesis wait stream-exists --stream-name ${stream_name}
 
-echo "consuming from ${stream_name} (ctrl+c to end)"
+echo "consuming from stream ${stream_name} (ctrl+c to end)"
 iterator=$(aws kinesis get-shard-iterator --stream-name ${stream_name} --shard-id 0 --shard-iterator-type TRIM_HORIZON --query '[ShardIterator]' --output text)
 for (( c=1; ; c++ )); do
   echo "iteration: ${c}"
   results=$(aws kinesis get-records --shard-iterator ${iterator})
   iterator=$(echo ${results} | jq .NextShardIterator --raw-output)  
   echo ${results} | jq '.Records[].Data' --raw-output | while read data; do
-    echo $(base64 --decode <<< ${data})
+    echo "consumed record $(base64 --decode <<< ${data})"
   done
 done
