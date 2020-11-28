fa2ab157561ce70599f827e7758487b87a3dab57
Alan McGinlay <amcginlay@users.noreply.github.com>
2020-11-28 15:02:23 +0000

    Update producer.sh

--- producer.sh
+++ producer.sh
@@ -12,27 +12,26 @@ echo "start './consumer.sh ${stream_id}' in another terminal session"
 echo "----------------------------------------------------------"
 echo
 
-echo "checking for: ${stream_name}"
+echo "checking for stream ${stream_name}"
 if aws kinesis describe-stream --stream-name ${stream_name} > /dev/null 2>&1; then
-  echo "deleting: ${stream_name}"
+  echo "deleting stream ${stream_name}"
   aws kinesis delete-stream --stream-name ${stream_name}
   aws kinesis wait stream-not-exists --stream-name ${stream_name}
-  echo "deleted: ${stream_name}"
+  echo "deleted stream ${stream_name}"
 fi
-echo "creating: ${stream_name}"
+echo "creating stream ${stream_name}"
 aws kinesis create-stream --stream-name ${stream_name} --shard-count 1
 aws kinesis wait stream-exists --stream-name ${stream_name}
-echo "created: ${stream_name}"
+echo "created stream ${stream_name}"
 
-echo "putting records in: ${stream_name}"
 for outer in $(seq ${outer_range}); do
   records=""
   for inner in $(seq ${inner_range}); do
     record="Data=$(base64 <<< ${outer}.${inner}),PartitionKey=${RANDOM}"
     records="${records} ${record}"
   done
-  echo "producing batch ${outer} of ${outer_range}"
+  echo "producing batch ${outer} of ${outer_range} in stream ${stream_name}"
   aws kinesis put-records --stream-name ${stream_name} --records ${records} > /dev/null 2>&1
-  echo "sleeping for ${sleep_duration} seconds"
+  echo "sleeping for ${sleep_duration} seconds ..."
   sleep ${sleep_duration}
 done
